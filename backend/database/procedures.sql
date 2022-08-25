2e6d618cce1385f10d79b9f6af3ed543829c04c7
Harris <harriskarume.hk@gmail.com>
2022-08-25 15:39:01 +0300

    Add procedures

---
+++ backend/database/procedures.sql
@@ -0,0 +1,49 @@
+-- get all blogs
+-- create or alter procedure getAllBlogs
+-- as
+-- begin
+
+-- select * from blog where deletedat = null
+
+-- end
+
+-- get blog
+-- create or alter procedure getBlog @id varchar(255)
+-- as
+-- begin
+
+-- select * from blog where id = @id
+
+-- end
+
+-- create blog
+-- create or alter procedure createBlog @id varchar(255), @title varchar(255), @body nvarchar(max)
+-- as
+-- begin
+
+-- insert into blog(id, title, body)
+-- values(@id, @title, @body)
+
+-- end
+
+-- update blog
+-- create or alter procedure updateBlog @id varchar(255), @title varchar(255), @body nvarchar(max), @modifiedat date
+-- as
+-- begin
+
+-- update blog
+-- set title = @title, body = @body, modifiedat = @modifiedat
+-- where id = @id
+
+-- end
+
+-- delete blog
+-- create or alter procedure deleteBlog @id varchar(255), @deletedat date
+-- as
+-- begin
+
+-- update blog 
+-- set deletedat = @deletedat
+-- where id = @id
+
+-- end
